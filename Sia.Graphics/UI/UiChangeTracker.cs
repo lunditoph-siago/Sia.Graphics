bf06a926b3e01597009d4932696e4acce992f2a3
Sieluna <seele.peng@gmail.com>
2026-08-14 00:38:38 +0300

    preserve ui slot topology across layout changes

--- Sia.Graphics/UI/UiChangeTracker.cs
+++ Sia.Graphics/UI/UiChangeTracker.cs
@@ -21,7 +21,6 @@ public sealed class UiChangeTracker : IAddon
     {
         LayoutVersion++;
         RenderVersion++;
-        RenderStructureVersion++;
     }
 
     public void MarkRenderDirty()

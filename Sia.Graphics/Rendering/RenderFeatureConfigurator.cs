c41ffcd17bbe614025ed6ac94e35e0fcf8a896c1
Sieluna <seele.peng@gmail.com>
2026-09-05 07:20:20 +0300

    remove engine feature orchestration from graphics

--- Sia.Graphics/Rendering/RenderFeatureConfigurator.cs
+++
@@ -1,9 +0,0 @@
-using Sia.Graphics.Reactive;
-using Sia.Reactive;
-
-namespace Sia.Graphics.Rendering;
-
-public delegate void RenderFeatureConfigurator<TContext>(
-    ref Hooks hooks,
-    WgpuRenderGraphRegistry registry,
-    in TContext context);
